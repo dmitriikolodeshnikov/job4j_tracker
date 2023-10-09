c0b1fd7e339b7e3061fd53652f0862791925462e
dmitriikolodeshnikov <kolodeshnikov-90@mail.ru>
2023-10-09 20:50:43 +0300

    6. Правила Checkstyle [#504981 #451133]

--- src/main/java/ru/job4j/checkstyle/Broken.java
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -2,11 +2,15 @@ package ru.job4j.checkstyle;
 
 public class Broken {
     String name;
+
     private int sizeOfEmpty = 10;
+
+    public static final String NEW_VALUE = "";
 
     public String surname;
 
-    public static final String NEW_VALUE = "";
+    Broken() {
+    }
 
     void echo() {
     }
@@ -20,8 +24,4 @@ public class Broken {
     void method(int a, int b, int c, int d, int e, int f, int g) {
 
     }
-
-    Broken() {
-    }
-
 }
