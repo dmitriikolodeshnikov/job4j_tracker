fb219bae5fd703eeff8ef2ce7336af87f117dad1
dmitriikolodeshnikov <kolodeshnikov-90@mail.ru>
2023-10-11 21:21:38 +0300

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,33 @@
+package ru.job4j.oop;
+
+public class Error {
+    private boolean active;
+    private int status;
+    private String message;
+
+    public Error() {
+        active = true;
+        status = 400;
+        message = "Ошибка";
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void printInfo() {
+        System.out.println(active);
+        System.out.println(status);
+        System.out.println(message);
+    }
+
+    public static void main(String[] args) {
+        Error error = new Error();
+        Error err = new Error(true, 500, "недоступен");
+        error.printInfo();
+        err.printInfo();
+
+    }
+}
