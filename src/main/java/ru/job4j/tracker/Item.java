ce9ac205f302f25c7c8b0d43ede0f954dc60719e
dmitriikolodeshnikov <kolodeshnikov-90@mail.ru>
2023-11-08 22:10:57 +0300

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -12,6 +12,7 @@ public class Item {
     }
 
     public Item(String name) {
+
         this.name = name;
     }
 
@@ -21,18 +22,22 @@ public class Item {
     }
 
     public int getId() {
+
         return id;
     }
 
     public void setId(int id) {
+
         this.id = id;
     }
 
     public String getName() {
+
         return name;
     }
 
     public void setName(String name) {
+
         this.name = name;
     }
 
