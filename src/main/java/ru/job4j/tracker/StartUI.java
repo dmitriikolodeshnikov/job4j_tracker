9984d6e62936bb691e9e12668b7b7d8097a2ed36
dmitriikolodeshnikov <kolodeshnikov-90@mail.ru>
2023-11-09 22:04:13 +0300

    10. toString [#310018]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -5,9 +5,7 @@ import java.time.format.DateTimeFormatter;
 
 public class StartUI {
     public static void main(String[] args) {
-        LocalDateTime item = new Item().getCreated();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-        String itemFormat = item.format(formatter);
-        System.out.println(itemFormat);
+        Item item = new Item();
+        System.out.println(item.toString());
     }
 }
