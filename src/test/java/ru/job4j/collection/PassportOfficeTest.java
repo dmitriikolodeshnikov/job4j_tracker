3cc2819ec64d0892fe412062e96433c42b22fbd3
dmitriikolodeshnikov <kolodeshnikov-90@mail.ru>
2026-03-10 13:12:23 +0300

    1. Паспорт и жители [#212656]

--- src/test/java/ru/job4j/collection/PassportOfficeTest.java
+++ src/test/java/ru/job4j/collection/PassportOfficeTest.java
@@ -11,4 +11,13 @@ public class PassportOfficeTest {
         office.add(citizen);
         assertThat(office.get(citizen.getPassport())).isEqualTo(citizen);
     }
+
+    @Test
+    public void whenTestSamePassport() {
+        Citizen citizen = new Citizen("2f44a", "Petr Arsentev");
+        Citizen same = new Citizen("2f44a", "Petr Arsentev");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen);
+        assertThat(office.add(same)).isFalse();
+    }
 }
