c5b6fafdbd0c49b2f1b08e8a2842d394ff86282e
dmitriikolodeshnikov <kolodeshnikov-90@mail.ru>
2026-03-12 22:46:51 +0300

    0. Map, HashMap [#212652 #559694]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -7,6 +7,9 @@ public class UsageMap {
         HashMap<String, String> map = new HashMap<>();
         map.put("ivanov@mail.ru", "Ivanov Ivan Ivanovich");
         map.put("petrov@mail.ru", "Petrov Petr Ivanovich");
+        map.put("sidorov@mail.ru", "Sidorov Petr Ivanovich");
+        map.put("vasiliy@mail.ru", "Sidorov Vasiliy Ivanovich");
+        map.put("ivanov@mail.ru", "Ivanov Petr Ivanovich");
         for (String key : map.keySet()) {
             String value = map.get(key);
             System.out.println(key + " " + value);
