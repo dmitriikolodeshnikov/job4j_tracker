e7bdd245707adfb5c053815d9818beaffb67c832
dmitriikolodeshnikov <kolodeshnikov-90@mail.ru>
2026-03-22 21:55:13 +0300

    4. Сортировка номера [#221284 #560593]

--- src/main/java/ru/job4j/collection/LexSort.java
+++ src/main/java/ru/job4j/collection/LexSort.java
@@ -5,14 +5,8 @@ import java.util.Comparator;
 public class LexSort implements Comparator<String> {
     @Override
     public int compare(String left, String right) {
-        String[] leftStr = left.split(". ");
-        String[] rightStr = right.split(". ");
-        int minLength = Math.min(leftStr.length, rightStr.length);
-        for (int i = 0; i < minLength; i++) {
-            if (!leftStr[i].equals(rightStr[i])) {
-                return Integer.compare(Integer.parseInt(leftStr[i]), Integer.parseInt(rightStr[i]));
-            }
-        }
-        return 0;
+        String[] leftStr = left.split("\\.");
+        String[] rightStr = right.split("\\.");
+        return Integer.compare(Integer.parseInt(leftStr[0]), Integer.parseInt(rightStr[0]));
     }
 }
