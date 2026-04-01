2ea8fff6227a58a3df8832174eb78b6905bec248
dmitriikolodeshnikov <kolodeshnikov-90@mail.ru>
2026-04-02 00:11:41 +0300

    9. Генерирование колоды карт. FlatMap [#504873]

---
+++ src/main/java/ru/job4j/stream/DeskOfCards.java
@@ -0,0 +1,13 @@
+package ru.job4j.stream;
+
+import java.util.stream.Stream;
+
+public class DeskOfCards {
+    public static void main(String[] args) {
+        Stream.of(Suit.values())
+                .flatMap(suit -> Stream.of(Value.values())
+                        .map(value -> suit + " " + value))
+                .forEach(System.out::println);
+
+    }
+}
